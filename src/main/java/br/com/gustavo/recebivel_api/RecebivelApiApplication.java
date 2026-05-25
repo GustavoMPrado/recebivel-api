6d1cb23dc711603e7e186f225541cffd9ff2e2f2
Gustavo Marinho Prado <gmarinhoprado@gmail.com>
2026-05-25 11:42:16 -0300

    chore: configurando projeto Spring Boot com PostgreSQL

---
+++ src/main/java/br/com/gustavo/recebivel_api/RecebivelApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.gustavo.recebivel_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecebivelApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecebivelApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gustavo/recebivel_api/RecebivelApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gustavo.recebivel_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecebivelApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
