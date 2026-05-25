258520f0a55e7ab30f75683cd689a5e65fe179a4
Gustavo Marinho Prado <gmarinhoprado@gmail.com>
2026-05-25 11:42:17 -0300

    feat: adicionando serviço de cliente

---
+++ src/main/java/br/com/gustavo/recebivel/cliente/ClienteService.java
@@ -0,0 +1,22 @@
+package br.com.gustavo.recebivel.cliente;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ClienteService {
+    private final ClienteRepository clienteRepository;
+
+    public ClienteService(ClienteRepository clienteRepository) {
+        this.clienteRepository = clienteRepository;
+    }
+
+    public List<Cliente> listar() {
+        return clienteRepository.findAll();
+    }
+
+    public Cliente salvar(Cliente cliente) {
+        return clienteRepository.save(cliente);
+    }
+}
