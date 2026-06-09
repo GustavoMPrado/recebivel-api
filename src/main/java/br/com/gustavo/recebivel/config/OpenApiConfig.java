28aa954712235b3872f7e2d697d7036aba9dc1e8
Gustavo Marinho Prado <gmarinhoprado@gmail.com>
2026-06-09 10:45:28 -0300

    chore: configurando jwt no swagger

--- src/main/java/br/com/gustavo/recebivel/config/OpenApiConfig.java
+++ src/main/java/br/com/gustavo/recebivel/config/OpenApiConfig.java
@@ -1,7 +1,10 @@
 package br.com.gustavo.recebivel.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,10 +13,19 @@ public class OpenApiConfig {
 
     @Bean
     public OpenAPI recebivelApi() {
+        String securitySchemeName = "bearerAuth";
+
         return new OpenAPI()
                 .info(new Info()
                         .title("Recebível API")
                         .description("API para controle interno de cobranças, parcelas, vencimentos e pagamentos.")
-                        .version("1.0.0"));
+                        .version("1.0.0"))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                                .name(securitySchemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
     }
 }
